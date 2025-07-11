8204c4214e65dd500939a69e765a74fa09ec9dee
JeongSina <jsi0613@kist.re.kr>
2025-07-11 19:13:19 +0900

    initial cleaning : training_utils.py

--- src/util/training_utils.py
+++ src/util/training_utils.py
@@ -54,67 +54,7 @@ def load_model(model_class, model_params:Dict, args):
     return model, optimizer, epoch, train_loss, valid_loss
 
 
-def run_an_epoch_x0(model,optimizer,loader,noiser,train:bool,args) -> List:
-    """
-    training, validation 1 epoch 진행
-    for DDPM
-    
-    Args:
-        model : 사용하는 모델
-        optimizer : optimizer
-        loader : dataloader
-        noiser (class): Diffusion
-        train (bool): True of False
-        args (function): t_dim(time embedding dimension) parsed
-    
-    Returns:
-        List : temp_loss 
-    """
-    temp_loss = {'total':[], 'loss1':[]}
-    device = args.device
-    t_dim = args.t_dim
-    model = model.to(device)
-    
-    for i,G in enumerate(loader):
-        if train: optimizer.zero_grad()
-        if not G or G == None: continue
-        G = G.to(device) 
-        pepidx = G.pepidx.clone()   
-        
-        ddpm = noiser(device, args.timestep, G)
-        base, used_alpha_bars_xyz, idxs = ddpm.time_embedding(t_dim, None)
-
-        xyz = G.node_xyz.clone()
-        xyz_label = xyz[G.pepidx].clone()
-        
-        G.node_attr = torch.cat([G.node_attr,base],dim=1)
-
-        x_tilde, alpha_bars_xyz, _ = ddpm.forward_process(used_alpha_bars_xyz, pepidx)
-        
-        Loss_weight_xyz = torch.clamp(alpha_bars_xyz / (1 - alpha_bars_xyz + 1e-7), max=5.0) 
-
-        G.node_xyz[pepidx] = x_tilde
-        G = dist_edges(G)
-
-        if train:
-            _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)  
-
-            loss = torch.mean(F.mse_loss(output[pepidx], xyz_label, reduction='none') * Loss_weight_xyz[pepidx])
-
-            loss.backward()
-            optimizer.step()
-            
-        else:
-            with torch.no_grad():
-                _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)
-
-                loss = torch.mean(F.mse_loss(output[pepidx], xyz_label, reduction='none') * Loss_weight_xyz[pepidx])
-
-        temp_loss["total"].append(loss.cpu().detach().numpy())
-
-    return temp_loss
-
-def run_an_epoch_x0pdl(model,optimizer,loader,noiser,train:bool,args) -> List:
+def run_an_epoch(model,optimizer,loader,noiser,train:bool,args) -> List:
     """
     training, validation 1 epoch 진행
     for DDPM
@@ -131,39 +71,40 @@ def run_an_epoch_x0pdl(model,optimizer,loader,noiser,train:bool,args) -> List:
         List : temp_loss 
     """
     temp_loss = {'total':[], 'loss1':[], 'loss2':[]}
-    device = args.device
-    t_dim = args.t_dim
-    model = model.to(device)
+    model = model.to(args.device)
     
     for i,G in enumerate(loader):
         if train: optimizer.zero_grad()
         if not G or G == None: continue
-        G = G.to(device) 
+        G = G.to(args.device) 
         pepidx = G.pepidx.clone()   
         
-        ddpm = noiser(device, args.timestep, G)
-        base, used_alpha_bars_xyz, idxs = ddpm.time_embedding(t_dim, None)
+        ddpm = noiser(args.device, args.timestep, G)
+        t_embed, used_alpha_bars = ddpm.time_embedding(args.t_dim, None)
 
         xyz = G.node_xyz.clone()
         xyz_label = xyz[G.pepidx].clone()
         
-        G.node_attr = torch.cat([G.node_attr,base],dim=1)
+        G.node_attr = torch.cat([G.node_attr,t_embed],dim=1)
 
-        x_tilde, alpha_bars_xyz, _ = ddpm.forward_process(used_alpha_bars_xyz, pepidx)
+        x_tilde = ddpm.forward_process(used_alpha_bars)
         
-        Loss_weight_xyz = torch.clamp(alpha_bars_xyz / (1 - alpha_bars_xyz + 1e-7), max=5.0) 
+        snr_weight = torch.clamp(used_alpha_bars / (1 - used_alpha_bars + 1e-7), max=5.0) 
 
-        original_distmap,_,_,_,_ = self_distmap(xyz, G.batch, G.pepidx2, idxs, args.timestep*0.25)
+        original_dist_value = pad_xyz_by_idx_condition(xyz, G.batch, G.target_idx2, torch.sqrt(snr_weight))
 
         G.node_xyz[pepidx] = x_tilde
         G = dist_edges(G)
 
         if train:
-            _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)  
-            output_distmap,_,_,_,_ = self_distmap(output, G.batch, G.pepidx2, idxs, args.timestep*0.25)
+            _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)
 
-            loss1 = torch.mean(F.mse_loss(output[pepidx], xyz_label, reduction='none') * Loss_weight_xyz[pepidx])
-            loss2 = F.mse_loss(original_distmap, output_distmap)
+            output_xyz = xyz.clone() 
+            output_xyz[G.pepidx] = output[G.pepidx]
+            output_dist_value = pad_xyz_by_idx_condition(output_xyz, G.batch, G.target_idx2, torch.sqrt(snr_weight))
+
+            loss1 = torch.mean(F.mse_loss(output[pepidx], xyz_label, reduction='none') * snr_weight[pepidx])
+            loss2 = F.mse_loss(original_dist_value, output_dist_value)
 
             loss = loss1 + loss2
             loss.backward()
@@ -172,293 +113,14 @@ def run_an_epoch_x0pdl(model,optimizer,loader,noiser,train:bool,args) -> List:
         else:
             with torch.no_grad():
                 _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)
-                output_distmap,_,_,_,_ = self_distmap(output, G.batch, G.pepidx2, idxs, args.timestep*0.25)
 
-                loss1 = torch.mean(F.mse_loss(output[pepidx], xyz_label, reduction='none') * Loss_weight_xyz[pepidx])
-                loss2 = F.mse_loss(original_distmap, output_distmap)
+                output_xyz = xyz.clone() 
+                output_xyz[G.pepidx] = output[G.pepidx]
+                output_dist_value = pad_xyz_by_idx_condition(output_xyz, G.batch, G.target_idx2, torch.sqrt(snr_weight))
 
-                loss = loss1 + loss2
+                loss1 = torch.mean(F.mse_loss(output[pepidx], xyz_label, reduction='none') * snr_weight[pepidx])
+                loss2 = F.mse_loss(original_dist_value, output_dist_value)
 
-        temp_loss["total"].append(loss.cpu().detach().numpy())
-        temp_loss["loss1"].append(loss1.cpu().detach().numpy())
-        temp_loss["loss2"].append(loss2.cpu().detach().numpy())
-
-    return temp_loss
-
-
-def run_an_epoch_x0pdlvar(model,optimizer,loader,noiser,train:bool,args) -> List:
-    """
-    training, validation 1 epoch 진행
-    for DDPM
-    
-    Args:
-        model : 사용하는 모델
-        optimizer : optimizer
-        loader : dataloader
-        noiser (class): Diffusion
-        train (bool): True of False
-        args (function): t_dim(time embedding dimension) parsed
-    
-    Returns:
-        List : temp_loss 
-    """
-    temp_loss = {'total':[], 'loss1':[], 'loss2':[], 'loss3':[], 'loss4':[]}
-    device = args.device
-    t_dim = args.t_dim
-    model = model.to(device)
-    
-    for i,G in enumerate(loader):
-        if train: optimizer.zero_grad()
-        if not G or G == None: continue
-        G = G.to(device) 
-        pepidx = G.pepidx.clone()   
-        
-        ddpm = noiser(device, args.timestep, G)
-        base, used_alpha_bars_xyz, idxs = ddpm.time_embedding(t_dim, None)
-
-        xyz = G.node_xyz.clone()
-        xyz_label = xyz[G.pepidx].clone()
-        
-        G.node_attr = torch.cat([G.node_attr,base],dim=1)
-
-        x_tilde, alpha_bars_xyz, _ = ddpm.forward_process(used_alpha_bars_xyz, pepidx)
-        
-        Loss_weight_xyz = torch.clamp(alpha_bars_xyz / (1 - alpha_bars_xyz + 1e-7), max=5.0) 
-
-        original_distmap, oCACAdist, oCACBdist, oCACAvar, oCACBvar = self_distmap(xyz, G.batch, G.pepidx2, idxs, args.timestep*0.25)
-
-        G.node_xyz[pepidx] = x_tilde
-        G = dist_edges(G)
-
-        if train:
-            _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)  
-            output_distmap, pCACAdist, pCACBdist, pCACAvar, pCACBvar = self_distmap(output, G.batch, G.pepidx2, idxs, args.timestep*0.25)
-
-            loss1 = torch.mean(F.mse_loss(output[pepidx], xyz_label, reduction='none') * Loss_weight_xyz[pepidx])
-            loss2 = F.mse_loss(original_distmap, output_distmap)
-            loss3 = F.mse_loss(oCACAdist, pCACAdist) + F.mse_loss(oCACBdist, pCACBdist) 
-            loss4 = F.mse_loss(oCACAvar, pCACAvar) + F.mse_loss(oCACBvar, pCACBvar)*10
-
-            loss = loss1 + loss2 + loss3 + loss4
-            loss.backward()
-            optimizer.step()
-            
-        else:
-            with torch.no_grad():
-                _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)
-                output_distmap, pCACAdist, pCACBdist, pCACAvar, pCACBvar = self_distmap(output, G.batch, G.pepidx2, idxs, args.timestep*0.25)
-
-                loss1 = torch.mean(F.mse_loss(output[pepidx], xyz_label, reduction='none') * Loss_weight_xyz[pepidx])
-                loss2 = F.mse_loss(original_distmap, output_distmap)
-                loss3 = F.mse_loss(oCACAdist, pCACAdist) + F.mse_loss(oCACBdist, pCACBdist) 
-                loss4 = F.mse_loss(oCACAvar, pCACAvar) + F.mse_loss(oCACBvar, pCACBvar)*10
-
-                loss = loss1 + loss2 + loss3 + loss4
-
-        temp_loss["total"].append(loss.cpu().detach().numpy())
-        temp_loss["loss1"].append(loss1.cpu().detach().numpy())
-        temp_loss["loss2"].append(loss2.cpu().detach().numpy())
-        temp_loss["loss3"].append(loss3.cpu().detach().numpy())
-        temp_loss["loss4"].append(loss4.cpu().detach().numpy())
-
-    return temp_loss
-
-def run_an_epoch_x0pdlvar2(model,optimizer,loader,noiser,train:bool,args) -> List:
-    """
-    training, validation 1 epoch 진행
-    for DDPM
-    
-    Args:
-        model : 사용하는 모델
-        optimizer : optimizer
-        loader : dataloader
-        noiser (class): Diffusion
-        train (bool): True of False
-        args (function): t_dim(time embedding dimension) parsed
-    
-    Returns:
-        List : temp_loss 
-    """
-    temp_loss = {'total':[], 'loss1':[], 'loss2':[], 'loss3':[]}
-    device = args.device
-    t_dim = args.t_dim
-    model = model.to(device)
-    
-    for i,G in enumerate(loader):
-        if train: optimizer.zero_grad()
-        if not G or G == None: continue
-        G = G.to(device) 
-        pepidx = G.pepidx.clone()   
-        
-        ddpm = noiser(device, args.timestep, G)
-        base, used_alpha_bars_xyz, idxs = ddpm.time_embedding(t_dim, None)
-
-        xyz = G.node_xyz.clone()
-        xyz_label = xyz[G.pepidx].clone()
-        
-        G.node_attr = torch.cat([G.node_attr,base],dim=1)
-
-        x_tilde, alpha_bars_xyz, _ = ddpm.forward_process(used_alpha_bars_xyz, pepidx)
-        
-        Loss_weight_xyz = torch.clamp(alpha_bars_xyz / (1 - alpha_bars_xyz + 1e-7), max=5.0) 
-
-        original_distmap, oCACAdist, oCACBdist, oCACAvar, oCACBvar = self_distmap(xyz, G.batch, G.pepidx2, idxs, args.timestep*0.25)
-
-        G.node_xyz[pepidx] = x_tilde
-        G = dist_edges(G)
-
-        if train:
-            _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)  
-            output_distmap, pCACAdist, pCACBdist, pCACAvar, pCACBvar = self_distmap(output, G.batch, G.pepidx2, idxs, args.timestep*0.25)
-
-            loss1 = torch.mean(F.mse_loss(output[pepidx], xyz_label, reduction='none') * Loss_weight_xyz[pepidx])
-            loss2 = F.mse_loss(original_distmap, output_distmap) 
-            loss3 = F.mse_loss(oCACAvar, pCACAvar) + F.mse_loss(oCACBvar, pCACBvar)*10
-
-            loss = loss1 + loss2 + loss3
-            loss.backward()
-            optimizer.step()
-            
-        else:
-            with torch.no_grad():
-                _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)
-                output_distmap, pCACAdist, pCACBdist, pCACAvar, pCACBvar = self_distmap(output, G.batch, G.pepidx2, idxs, args.timestep*0.25)
-
-                loss1 = torch.mean(F.mse_loss(output[pepidx], xyz_label, reduction='none') * Loss_weight_xyz[pepidx])
-                loss2 = F.mse_loss(original_distmap, output_distmap)
-                loss3 = F.mse_loss(oCACAvar, pCACAvar) + F.mse_loss(oCACBvar, pCACBvar)*10
-
-                loss = loss1 + loss2 + loss3 
-
-        temp_loss["total"].append(loss.cpu().detach().numpy())
-        temp_loss["loss1"].append(loss1.cpu().detach().numpy())
-        temp_loss["loss2"].append(loss2.cpu().detach().numpy())
-        temp_loss["loss3"].append(loss3.cpu().detach().numpy())
-
-    return temp_loss
-
-def run_an_epoch_ep(model,optimizer,loader,noiser,train:bool,args) -> List:
-    """
-    training, validation 1 epoch 진행
-    for DDPM
-    
-    Args:
-        model : 사용하는 모델
-        optimizer : optimizer
-        loader : dataloader
-        noiser (class): Diffusion
-        train (bool): True of False
-        args (function): t_dim(time embedding dimension) parsed
-    
-    Returns:
-        List : temp_loss 
-    """
-    temp_loss = {'total':[]}
-    device = args.device
-    t_dim = args.t_dim
-    model = model.to(device)
-    
-    for i,G in enumerate(loader):
-        if train: optimizer.zero_grad()
-        if not G or G == None: continue
-        G = G.to(device) 
-        pepidx = G.pepidx.clone() 
-        # G.node_xyz = G.node_xyz * 10  
-        
-        ddpm = noiser(device, args.timestep, G)
-        base, used_alpha_bars_xyz, idxs = ddpm.time_embedding(t_dim, None)
-        
-        G.node_attr = torch.cat([G.node_attr,base],dim=1)
-
-        x_tilde, _, epsilon = ddpm.forward_process(used_alpha_bars_xyz, pepidx)
-        x_t = x_tilde.clone()
-        G.node_xyz[pepidx] = x_tilde
-        
-        G = dist_edges(G)
-
-        if train:
-            _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)  
-
-            loss = F.mse_loss(output[pepidx], epsilon)
-
-            loss.backward()
-            optimizer.step()
-            
-        else:
-            with torch.no_grad():
-                _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)
-
-                loss = F.mse_loss(output[pepidx], epsilon)
-
-        temp_loss["total"].append(loss.cpu().detach().numpy())
-
-    return temp_loss
-
-
-def run_an_epoch_eppdl(model,optimizer,loader,noiser,train:bool,args) -> List:
-    """
-    training, validation 1 epoch 진행
-    for DDPM
-    
-    Args:
-        model : 사용하는 모델
-        optimizer : optimizer
-        loader : dataloader
-        noiser (class): Diffusion
-        train (bool): True of False
-        args (function): t_dim(time embedding dimension) parsed
-    
-    Returns:
-        List : temp_loss 
-    """
-    temp_loss = {'total':[], 'loss1':[], 'loss2': []}
-    device = args.device
-    t_dim = args.t_dim
-    model = model.to(device)
-    
-    for i,G in enumerate(loader):
-        if train: optimizer.zero_grad()
-        if not G or G == None: continue
-        G = G.to(device) 
-        pepidx = G.pepidx.clone()   
-
-        original_xyz = G.node_xyz.clone() 
-        
-        ddpm = noiser(device, args.timestep, G)
-        base, used_alpha_bars_xyz, idxs = ddpm.time_embedding(t_dim, None)
-
-        odistmap = get_distmap(original_xyz, G.batch, G.pepidx2, idxs, args.timestep*0.25)
-        
-        G.node_attr = torch.cat([G.node_attr,base],dim=1)
-
-        x_tilde, _, epsilon = ddpm.forward_process(used_alpha_bars_xyz, pepidx)
-        G.node_xyz[pepidx] = x_tilde
-        x_t = G.node_xyz.clone()
-        
-        G = dist_edges(G)
-
-        if train:
-            _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr) 
-            x0_pred = get_x0(used_alpha_bars_xyz, output, x_t)
-            x_t[pepidx] = x0_pred[pepidx]
-            pdistmap = get_distmap(x_t, G.batch, G.pepidx2, idxs, args.timestep*0.25)
-
-            loss1 = F.mse_loss(output[pepidx], epsilon)
-            loss2 = F.mse_loss(pdistmap, odistmap)
-            loss = loss1 + loss2
-            
-            loss.backward()
-            optimizer.step()
-            
-        else:
-            with torch.no_grad():
-                _,output = model(G.node_attr, G.node_xyz, G.edge_index, G.edge_attr)
-                x0_pred = get_x0(used_alpha_bars_xyz, output, x_t)
-                x_t[pepidx] = x0_pred[pepidx]
-                pdistmap = get_distmap(x_t, G.batch, G.pepidx2, idxs, args.timestep*0.25)
-
-                loss1 = F.mse_loss(output[pepidx], epsilon)
-                loss2 = F.mse_loss(pdistmap, odistmap)
                 loss = loss1 + loss2
 
         temp_loss["total"].append(loss.cpu().detach().numpy())
@@ -502,6 +164,58 @@ def save_model(epoch:int, model, optimizer, train_loss, valid_loss, args) -> Non
         }, args.model_path+'/'+args.model_name+"/model.pkl")
 
 
+def pad_xyz_by_idx_condition(xyz, G_batch, target_idx2, weights):
+
+    weighted_xyz = xyz*weights
+
+    graph_xyzs = []
+    max_nodes = 0
+    target_masks = []
+    total_masks = []
+
+    for g in G_batch.unique().tolist():
+        node_mask = (G_batch == g)
+        
+        this_xyz = weighted_xyz[node_mask]
+        graph_xyzs.append(this_xyz)
+        max_nodes = max(max_nodes, this_xyz.shape[0])
+
+        target_mask = torch.zeros(this_xyz.shape[0], dtype=torch.bool)
+        target_mask[target_idx2[g]] = 1
+        target_masks.append(target_mask)
+
+        total_mask = torch.ones(this_xyz.shape[0], dtype=torch.bool)
+        total_masks.append(total_mask)
+
+    # padding
+    padded = [
+        F.pad(x, (0, 0, 0, max_nodes - x.shape[0]), value=0.0)
+        for x in graph_xyzs
+    ]
+    padded_target_mask = [
+        F.pad(x, (0, max_nodes - x.shape[0]), value=0)
+        for x in target_masks
+    ]
+    padded_total_mask = [
+        F.pad(x, (0, max_nodes - x.shape[0]), value=0)
+        for x in total_masks
+    ]
+
+    if len(padded) == 0: value = torch.tensor(0.0)
+    else:
+        padded_xyz = torch.stack(padded)
+        padded_target_mask = torch.stack(padded_target_mask)
+        padded_total_mask = torch.stack(padded_total_mask)
+
+        m1 = padded_target_mask.unsqueeze(2) | padded_target_mask.unsqueeze(1)
+        m2 = padded_total_mask.unsqueeze(2) & padded_total_mask.unsqueeze(1)
+        mask = m1 & m2
+
+        distance_map = torch.cdist(padded_xyz, padded_xyz)
+        value = distance_map[mask]
+
+    return value
+
 def self_distmap(xyz, G_batch, pepidx2, idxs, timestep_crit):
     idxs = torch.tensor(idxs).to(device=xyz.device)
     distmap_list = [] 
@@ -536,31 +250,6 @@ def self_distmap(xyz, G_batch, pepidx2, idxs, timestep_crit):
         CACBvar = no_value.clone()
 
     return torch.cat(distmap_list), CACAdist, CACBdist, CACAvar, CACBvar
-
-
-def get_distmap(xyz, G_batch, pepidx2, idxs, timestep_crit):
-    idxs = torch.tensor(idxs).to(device=xyz.device)
-    distmap_list = [] 
-    for batch_num in G_batch.unique().tolist(): 
-        node_mask = (G_batch == batch_num)
-        
-        selected = idxs[node_mask]
-        assert torch.all(selected == selected[0]), "Batch idxs Timestep Error!"
-        timestep = idxs[node_mask][0]
-        if timestep > timestep_crit: continue 
-
-        pepidx = pepidx2[batch_num]
-        batch_xyz = xyz[node_mask]
-        pepxyz = batch_xyz[pepidx]
-
-        distmap = torch.cdist(pepxyz, pepxyz)
-        distmap_list.append(distmap.flatten())
-
-    if len(distmap_list) == 0:
-        no_value = torch.tensor([0.0])
-        distmap_list.append(no_value.clone())
-
-    return torch.cat(distmap_list)
 
 
 def dist_edges(G):
