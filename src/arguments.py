d15ed755ea35a768011500e49a4aa855edb1b78f
JeongSina <jsi0613@kist.re.kr>
2025-07-22 16:42:54 +0900

    nothing

---
+++ src/arguments.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+import torch
+
+def set_arguments():
+    parser = ArgumentParser()
+    
+    parser.add_argument('--device', type=str, default="cuda" if torch.cuda.is_available() else "cpu")
+    parser.add_argument('--project_path', type=str, default="/home/jsi0613/projects/ddpm_coarse/")
+
+    # dataset
+    parser.add_argument('--pkl_path', type=str, default='/scratch/jsi0613/CG_data/pickles/')
+    parser.add_argument('--t_dim', type=int, default=4, help='time embedding dimension')
+    
+    # model
+    parser.add_argument('--lr', type=float, default=3.0e-4)
+    parser.add_argument('--model_path', type=str, default='/home/jsi0613/projects/ddpm_coarse/weights/')
+    parser.add_argument('--MAXEPOCHS', type=int, default=1000)
+    parser.add_argument('--timestep', type=int, default=300)
+    parser.add_argument('--model_name', type=str, default='CG_300')
+
+    # sampling
+    parser.add_argument('-sample_path', type=str, default='/home/jsi0613/projects/ddpm_coarse/data/samples/')
+    
+    args = parser.parse_args()
+    
+    return args
